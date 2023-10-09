30e4f8578efdc88e52edcf63a4b839bdb81f3b18
fabio.vazzz <fabiohenriquevaz@hotmail.com>
2023-10-09 12:08:28 -0300

    first commit

---
+++ CalculadoraDominio/Calculadora.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDominio
+{
+    public class Calculadora
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        public int Multiply(int x, in int y)
+        {
+            return x * y;
+        }
+        public int Division(int x, in int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ CalculadoraTests/CalculadoraTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculadoraDominio;
+using NUnit.Framework;
+
+namespace CalculadoraTests
+{
+    [TestFixture]
+    public class CalculadoraTest
+    {
+        [Test]
+        public void TestSum()
+        {
+            //Arrange  levanta os dados para poder executar os testes
+            int a = 3;
+            int b = 2;
+            int r = 5;
+
+            //Act 
+            Calculadora calculadora = new();
+            int result = calculadora.Sum(a, b);
+
+            //Assert
+
+            Assert.AreEqual(result, r);
+        }
+        [Test]
+        public void TestSubtract()
+        {
+            int a = 3;
+            int b = 2;
+            int r = 1;
+             
+            Calculadora calculadora = new();
+            int result = calculadora.Subtract(a, b);
+
+            Assert.AreEqual(result, r);
+        }
+        [Test]
+        public void TestDivision()
+        {
+            int a = 10;
+            int b = 2;
+            int r = 5;
+
+            Calculadora calculadora = new();
+            int result = calculadora.Division(a, b);
+
+            Assert.AreEqual(result, r);
+        }
+        [Test]
+        public void TestMultiply()
+        {
+            int a = 3;
+            int b = 2;
+            int r = 6;
+
+            Calculadora calculadora = new();
+            int result = calculadora.Multiply(a, b);
+
+            Assert.AreEqual(result, r);
+        }
+    }
+}
